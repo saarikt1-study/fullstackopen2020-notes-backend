b68fa873c62ee62644d34304c9bf1844498c9360
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-14 11:25:05 +0200

    Add error handling

--- index.js
+++ index.js
@@ -12,37 +12,10 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('build'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  },
-  {
-    id: 4,
-    content: "Testi!!!",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -74,10 +47,16 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note.toJSON())
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+        response.status(404).end()
+      }
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -92,6 +71,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
